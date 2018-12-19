916b9c2f65eedf827cbd09abba3d562fc63ac19c
tundeUsman <tundeusman0@gmail.com>
2018-12-19 12:18:56 +0100

    Add GET /todo routes

--- server/server.js
+++ server/server.js
@@ -19,6 +19,12 @@ app.post('/todos',(req,res)=>{
         res.status(400).send(e);
     })
 })
+
+app.get('/todos',(req,res)=>{
+    Todo.find().then((todos)=>{
+        res.send({todos})
+    },(e)=>res.status(400).send(e))
+})
 app.listen(3000,()=>console.log(`connected to port 3000`))
 
 module.exports = {app};
