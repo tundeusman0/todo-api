b3692644a32475091300063539c3b90b6a28daf8
tundeUsman <tundeusman0@gmail.com>
2018-12-20 15:54:03 +0100

    Add test for Patch/todo:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -11,7 +11,9 @@ const todos = [{
 
 },{
     _id: new ObjectID(),
-    text:"Second test todo"
+    text:"Second test todo",
+    completed: true,
+    completedAt: 333
 }]
 
 beforeEach((done)=>{
@@ -139,18 +141,52 @@ describe('DELETE /Todos/:id',()=>{
     })
 })
 
-// describe('PATCH /Todos/:id',()=>{
-//     it('should update with a valid id',(done)=>{
-//         let id = todos[1]._id.toHexString()
-//         request(app)
-//             .patch(`/todos/${id}`)
-//             .expect(200)
-//             .expect((res)=>{
-//                 expect(res.body.todos).toBe(id)
-//                 
-//             })
-//             .end((err)=>{
-//                  err? done(err):
-//             })
-//     })
-// })
+describe('PATCH /Todos/:id',()=>{
+    it('should update with a valid id',(done)=>{
+        let id = todos[0]._id.toHexString()
+        let text = "this should be the new text"
+        let completed = true
+        request(app)
+            .patch(`/todos/${id}`)
+            .send({text,completed})
+            .expect(200)
+            .expect((res)=>{
+                expect(res.body.todo.text).toBe(text)   
+                expect(res.body.todo.completed).toBe(completed)   
+                expect(res.body.todo.completedAt).toBeA('number')   
+            })
+            .end((err)=>{
+                err ? done(err) :
+                Todo.find({ text, completed }).then((todos) => {
+                    expect(todos[0].text).toBe(text)
+                    expect(todos[0].completed).toBe(completed)
+                    expect(todos[0].completedAt).toBeA('number')
+                    done();
+                }).catch((e) => done(e))
+            })
+    })
+
+    it('should clear completedAt when todo is not completed',(done)=>{
+        let id = todos[1]._id.toHexString()
+        let text = "this should be the new text !!"
+        let completed = false
+        request(app)
+            .patch(`/todos/${id}`)
+            .send({ text,completed})
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text)
+                expect(res.body.todo.completed).toBe(completed)
+                expect(res.body.todo.completedAt).toNotExist()
+            })
+            .end((err) => {
+                err ? done(err) :
+                    Todo.find({ text, completed }).then((todos) => {
+                        expect(todos[0].text).toBe(text)
+                        expect(todos[0].completed).toBe(completed)
+                        expect(todos[0].completedAt).toNotExist()
+                        done();
+                    }).catch((e) => done(e))
+            })
+    })
+})
